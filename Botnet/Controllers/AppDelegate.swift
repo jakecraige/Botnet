8440a9d262bf353b5fc112bf00c395c5c4e159b0
Jake Craige <james.craige@gmail.com>
2016-06-14 14:17:38 -0700

    Initial Commit

---
+++ Botnet/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Botnet
+//
+//  Created by James Craige on 6/14/16
+//  Copyright (c) 2016 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
